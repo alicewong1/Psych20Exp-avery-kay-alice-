5d43ecab310a0d987dbc4377c134b473c7c12ea9
alicewong1 <sookmun.wong@ucla.edu>
2015-11-06 11:46:33 -0800

    added link for lesson on drawing dots

--- Psych20ExpAliceAveryKay.m
+++ Psych20ExpAliceAveryKay.m
@@ -1,3 +1,5 @@
 %experiment code space :D
 %change nubmer 2
 %chang3 
+% Nice lesson link for drawing dots:
+% http://www.mbfys.ru.nl/~robvdw/DGCN22/PRACTICUM_2011/LABS_2011/ALTERNATIVE_LABS/Lesson_2.html
