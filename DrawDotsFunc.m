927534b4630c996a2ec0133e2a885f1d4193cd2a
alicewong1 <sookmun.wong@ucla.edu>
2015-11-09 16:29:01 -0800

    change to drawdotfunc

--- DrawDotsFunc.m
+++ DrawDotsFunc.m
@@ -1,4 +1,10 @@
 function [output] = DrawDotsFunc(nFrames)
+    dotsize = 50;
+    xy = [dotsize (winRect(3)-dotsize);dotsize dotsize];
+    Screen('DrawDots',win,xy,[dotsize],[0 0 255],[0,0],[1]);
+    Screen('Flip',win);
+    WaitSecs(2);    
+
     moveDown = (winRect(3)-dotsize)/nFrames;
     moveSide = (winRect(4)-dotsize)/nFrames;
     for i =1:nFrames
--- Psych20ExpAliceAveryKay.m
+++ Psych20ExpAliceAveryKay.m
@@ -3,14 +3,11 @@
 %chang3 
 % Nice lesson link for drawing dots:
 % http://www.mbfys.ru.nl/~robvdw/DGCN22/PRACTICUM_2011/LABS_2011/ALTERNATIVE_LABS/Lesson_2.html
+
 sizeScreen = get(0,'ScreenSize');
 sizeScreen = [sizeScreen(3:end),sizeScreen(3:end)];
 [win,winRect] = Screen('OpenWindow',0,[255 255 255],sizeScreen.*[.2 .6 .8 .95]);
 
-dotsize = 50;
-xy = [dotsize (winRect(3)-dotsize);dotsize dotsize];
-Screen('DrawDots',win,xy,[dotsize],[0 0 255],[0,0],[1]);
-Screen('Flip',win);
-WaitSecs(2);
+
 
 
